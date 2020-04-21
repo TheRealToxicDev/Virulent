d3da8162919f679fb1a797fec815bce7bf6f7f59
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-04-21 05:35:20 -0600

    Fix: Gateway Heartbeat Issues. 💓

--- src/Gateway/WebSocket.js
+++ src/Gateway/WebSocket.js
@@ -9,7 +9,7 @@ const Payloads = require('./Payloads');
 
 module.exports = class WebSocket extends EventEmitter {
     constructor(client) {
-        super()
+        super();
         this._client = client;
         this.is_ready = false;
         this.is_disconnected = false;
@@ -19,13 +19,13 @@ module.exports = class WebSocket extends EventEmitter {
     }
 
     /**
-     * Connect the Client to the Discord Gateway
+     * Connects the client to the Discord Gateway
      * @param token
      */
     connect(token) {
         this._ws = new ws(Endpoints.GATEWAY + '/?v=' + Constants.GATEWAY_VERSION + '&encoding=json');
         this._ws.once('open', () => {
-            this._WSConnect(Payloads.IDENTIFY({token}));
+            this._WSConnect(Payloads.IDENTIFY({ token }));
         });
         this._ws.once('close', this._handleWSClose.bind(this));
         this._ws.once('error', this._handleWSError.bind(this));
@@ -33,7 +33,7 @@ module.exports = class WebSocket extends EventEmitter {
     }
 
     /**
-     * Disconnects the Client from the Discord Gateway
+     * Disconnects the client from the Discord Gateway
      */
     disconnect() {
         this.is_disconnected = true;
@@ -41,7 +41,7 @@ module.exports = class WebSocket extends EventEmitter {
     }
 
     /**
-     * Sends Payload to the Discord Gateway
+     * Sends payload to the Discord gateway
      * @param payload
      */
     WSSend(payload) {
@@ -63,71 +63,71 @@ module.exports = class WebSocket extends EventEmitter {
             case Constants.GATEWAY_OP_CODES.DISPATCH:
                 this._heartbeat = message.d.s;
                 break;
-                case Constants.GATEWAY_OP_CODES.HEARTBEAT:
-                    this.WSSend(Payloads.HEARTBEAT(this._heartbeat));
-                    break;
-                case Constants.GATEWAY_OP_CODES.RECONNECT:
-                    this._ws.terminate();
-                    break;
-                case Constants.GATEWAY_OP_CODES.HELLO:
-                    const token = this._client.token;
-                    if (this._client.presence !== null) {
-                        this.WSSend(Payloads.PRESENCE(this._client.presence));
-                    }
-                    let payload;
-                    if (this._sessionId !== null && this._heartbeat !== null) {
-                        payload = Payloads.RESUME({
-                            sequence: this._heartbeat,
-                            sessionId: this._sessionId,
-                            token: this._client.token
-                        });
-                    } else {
-                        payload = Payloads.IDENTIFY({ token });
-                    }
-                    this._WSConnect(payload);
-                    break;
-            }
-            switch (message.t) {
-                case "READY":
-                    if (!this.is_ready) {
-                        this.emit('ready', message.d.user);
-                        this.is_ready = true;
-                        this._sessionId = message.d.session_id;
-                    }
-                    break;
-                case "MESSAGE_CREATE":
-                    this.emit('message', message.d);
-                    break;
-            }
-        }
-    
-        _handleWSError(error) {
-            if (this._ws !== null) {
-                if (error) {
-                    throw error;
+            case Constants.GATEWAY_OP_CODES.HEARTBEAT:
+                this.WSSend(Payloads.HEARTBEAT(this._heartbeat));
+                break;
+            case Constants.GATEWAY_OP_CODES.RECONNECT:
+                this._ws.terminate();
+                break;
+            case Constants.GATEWAY_OP_CODES.HELLO:
+                const token = this._client.token;
+                if (this._client.presence !== null) {
+                    this.WSSend(Payloads.PRESENCE(this._client.presence));
                 }
-            }
-        }
-    
-        _handleWSClose(code, data) {
-            if (this._ws !== null && !this.is_disconnected) {
-                setTimeout(() => this.connect(this._client.token), 1000);
-            }
-        }
-    
-        _decompressWSMessage(message, flags) {
-            if (typeof flags !== 'object')
-                flags = {};
-            if (!flags.binary) {
-                return JSON.parse(message);
-            } else {
-                const inflate = new ZlibSync.Inflate();
-                inflate.push(message, ZlibSync.Z_SYNC_FLUSH);
-    
-                if(inflate.err < 0) {
-                    throw new Error('An error has occured with Zlib: ' + inflate.msg);
+                let payload;
+                if (this._sessionId !== null && this._heartbeat !== null) {
+                    payload = Payloads.RESUME({
+                        sequence: this._heartbeat,
+                        sessionId: this._sessionId,
+                        token: this._client.token
+                    });
+                } else {
+                    payload = Payloads.IDENTIFY({ token });
                 }
-                return JSON.parse(inflate.toString());
+                this._WSConnect(payload);
+                break;
+        }
+        switch (message.t) {
+            case "READY":
+                if (!this.is_ready) {
+                    this.emit('ready', message.d.user);
+                    this.is_ready = true;
+                    this._sessionId = message.d.session_id;
+                }
+                break;
+            case "MESSAGE_CREATE":
+                this.emit('message', message.d);
+                break;
+        }
+    }
+
+    _handleWSError(error) {
+        if (this._ws !== null) {
+            if (error) {
+                throw error;
             }
         }
-    };
+    }
+
+    _handleWSClose(code, data) {
+        if (this._ws !== null && !this.is_disconnected) {
+            setTimeout(() => this.connect(this._client.token), 1000);
+        }
+    }
+
+    _decompressWSMessage(message, flags) {
+        if (typeof flags !== 'object')
+            flags = {};
+        if (!flags.binary) {
+            return JSON.parse(message);
+        } else {
+            const inflate = new ZlibSync.Inflate();
+            inflate.push(message, ZlibSync.Z_SYNC_FLUSH);
+
+            if(inflate.err < 0) {
+                throw new Error('An error has occured with Zlib: ' + inflate.msg);
+            }
+            return JSON.parse(inflate.toString());
+        }
+    }
+};
